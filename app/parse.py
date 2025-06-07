58fac6f16369580ad168c2ff9c74de7d11d91699
Ivan Havrylyshyn <jyjuk1@gmail.com>
2025-06-07 09:17:13 +0300

    Solution

--- app/parse.py
+++ app/parse.py
@@ -1,4 +1,17 @@
+import csv
 from dataclasses import dataclass
+from typing import List
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://quotes.toscrape.com/"
+
+
+def get_page(url: str) -> BeautifulSoup:
+    response = requests.get(url=url)
+    response.raise_for_status()
+    return BeautifulSoup(response.text, "html.parser")
 
 
 @dataclass
@@ -8,8 +21,45 @@ class Quote:
     tags: list[str]
 
 
+def get_quotes(soup: BeautifulSoup) -> List[Quote]:
+    queue_list = []
+    for quote in soup(".quote"):
+        text = quote.select_one(".text").get_text(strip=True)
+        author = quote.select_one(".author").get_text(strip=True)
+        tags = [tag.get_text(strip=True) for tag in quote.select(".tag")]
+
+        queue_list.append(Quote(text=text, author=author, tags=tags))
+
+    return queue_list
+
+
+def get_all_quotes() -> List[Quote]:
+    quotes = []
+    url = URL
+
+    while url:
+        soup = get_page(url)
+        quotes.extend(get_quotes(soup))
+
+        next_btn = soup.select_one(".next a")
+        url = URL + next_btn["href"] if next_btn else None
+
+    return quotes
+
+
+def save_to_csv(quotes: List[Quote], output_csv_path: str) -> None:
+    with open(output_csv_path, "w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Text", "Author", "Tags"])
+
+        for quote in quotes:
+            writer.writerow([quote.text, quote.author, ", ".join(quote.tags)])
+
+
 def main(output_csv_path: str) -> None:
-    pass
+    quotes = get_all_quotes()
+    save_to_csv(quotes, output_csv_path)
+    print(f"✅ {len(quotes)} quotes saved to {output_csv_path}")
 
 
 if __name__ == "__main__":
